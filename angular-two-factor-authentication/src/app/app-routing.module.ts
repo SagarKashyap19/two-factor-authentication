45475f7d2bf11b1745797a7628a4e010381848d8
sagarjagadeesh <sagardrr@gmail.com>
2020-04-16 20:12:24 +0530

    Implemented sign in form

--- angular-two-factor-authentication/src/app/app-routing.module.ts
+++ angular-two-factor-authentication/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignInComponent } from './views/sign-in/sign-in.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: SignInComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular-two-factor-authentication/src/app/app.module.ts
+++ angular-two-factor-authentication/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignInComponent } from './views/sign-in/sign-in.component';
+import { UserProfileComponent } from './views/user-profile/user-profile.component';
+import { OtpComponent } from './views/otp/otp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent,
+    UserProfileComponent,
+    OtpComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
