f00dc253533764bcf3922475246ea6b61c1f83b0
sagarjagadeesh <sagardrr@gmail.com>
2020-04-16 21:56:46 +0530

    Validating form fields

--- angular-two-factor-authentication/src/app/views/sign-in/sign-in.component.ts
+++ angular-two-factor-authentication/src/app/views/sign-in/sign-in.component.ts
@@ -1,19 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { CustomValidators } from './custom-validators';
 
 @Component({
-  selector: 'app-sign-in',
-  templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.scss']
+  selector: "app-sign-in",
+  templateUrl: "./sign-in.component.html",
+  styleUrls: ["./sign-in.component.scss"]
 })
 export class SignInComponent implements OnInit {
+  public frmSignup: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {
+    this.frmSignup = this.createSignupForm();
+  }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  createSignupForm(): FormGroup {
+    return this.fb.group(
+      {
+        email: [
+          null,
+          Validators.compose([Validators.email, Validators.required])
+        ],
+        password: [
+          null,
+          Validators.compose([
+            Validators.required,
+            // check whether the entered password has a number
+            CustomValidators.patternValidator(/\d/, {
+              hasNumber: true
+            }),
+            // check whether the entered password has upper case letter
+            CustomValidators.patternValidator(/[A-Z]/, {
+              hasCapitalCase: true
+            }),
+            // check whether the entered password has a lower case letter
+            CustomValidators.patternValidator(/[a-z]/, {
+              hasSmallCase: true
+            }),
+            // check whether the entered password has a special character
+            CustomValidators.patternValidator(
+              /[ !@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/,
+              {
+                hasSpecialCharacters: true
+              }
+            ),
+            Validators.minLength(8)
+          ])
+        ],
+      },
+    );
   }
 
   submit() {
-    console.log("Submt")
+    console.log(this.frmSignup.value);
   }
-
 }
