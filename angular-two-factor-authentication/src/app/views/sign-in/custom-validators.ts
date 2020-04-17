8efcda468aff3c895979c3adbb9b654bfa16bfe2
sagarjagadeesh <sagardrr@gmail.com>
2020-04-17 23:05:47 +0530

    Sign up form validation

--- angular-two-factor-authentication/src/app/views/sign-in/custom-validators.ts
+++ angular-two-factor-authentication/src/app/views/sign-in/custom-validators.ts
@@ -17,12 +17,12 @@ export class CustomValidators {
   }
 
   static passwordMatchValidator(control: AbstractControl) {
-    const password: string = control.get('password').value; // get password from our password form control
-    const confirmPassword: string = control.get('confirmPassword').value; // get password from our confirmPassword form control
+    const password: string = control.get('Password').value; // get password from our password form control
+    const confirmPassword: string = control.get('ConfirmPassword').value; // get password from our confirmPassword form control
     // compare is the password math
     if (password !== confirmPassword) {
       // if they don't match, set an error in our confirmPassword form control
-      control.get('confirmPassword').setErrors({ NoPassswordMatch: true });
+      control.get('ConfirmPassword').setErrors({ NoPassswordMatch: true });
     }
   }
 }
--- angular-two-factor-authentication/src/app/views/signup/signup.component.ts
+++ angular-two-factor-authentication/src/app/views/signup/signup.component.ts
@@ -1,67 +1,73 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 import { CustomValidators } from "../sign-in/custom-validators";
 import { AuthService } from "src/app/services/auth.service";
 
+var bcrypt = require("bcryptjs");
+
 @Component({
-  selector: 'app-signup',
-  templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss']
+  selector: "app-signup",
+  templateUrl: "./signup.component.html",
+  styleUrls: ["./signup.component.scss"]
 })
 export class SignupComponent implements OnInit {
   public frmSignup: FormGroup;
-  public frmOtp: FormGroup;
-  public isOtp: boolean = false;
 
   constructor(private authService: AuthService, private fb: FormBuilder) {}
 
   ngOnInit(): void {
     this.frmSignup = this.createSignupForm();
-    this.frmOtp = this.fb.group({
-      otp: [''],
-    })
   }
 
   createSignupForm(): FormGroup {
-    return this.fb.group({
-      Email: [
-        null,
-        Validators.compose([Validators.email, Validators.required])
-      ],
-      Password: [
-        null,
-        Validators.compose([
-          Validators.nullValidator,
-          Validators.required,
-          // check whether the entered password has a number
-          CustomValidators.patternValidator(/\d/, {
-            hasNumber: true
-          }),
-          // check whether the entered password has upper case letter
-          CustomValidators.patternValidator(/[A-Z]/, {
-            hasCapitalCase: true
-          }),
-          // check whether the entered password has a lower case letter
-          CustomValidators.patternValidator(/[a-z]/, {
-            hasSmallCase: true
-          }),
-          // check whether the entered password has a special character
-          CustomValidators.patternValidator(
-            /[ !@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/,
-            {
-              hasSpecialCharacters: true
-            }
-          ),
-          Validators.minLength(8)
-        ])
-      ]
-    });
+    return this.fb.group(
+      {
+        Email: [
+          null,
+          Validators.compose([Validators.email, Validators.required])
+        ],
+        Password: [
+          null,
+          Validators.compose([
+            Validators.nullValidator,
+            Validators.required,
+            // check whether the entered password has a number
+            CustomValidators.patternValidator(/\d/, {
+              hasNumber: true
+            }),
+            // check whether the entered password has upper case letter
+            CustomValidators.patternValidator(/[A-Z]/, {
+              hasCapitalCase: true
+            }),
+            // check whether the entered password has a lower case letter
+            CustomValidators.patternValidator(/[a-z]/, {
+              hasSmallCase: true
+            }),
+            // check whether the entered password has a special character
+            CustomValidators.patternValidator(
+              /[ !@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/,
+              {
+                hasSpecialCharacters: true
+              }
+            ),
+            Validators.minLength(8)
+          ])
+        ],
+        ConfirmPassword: [null, Validators.compose([Validators.required])]
+      },
+      {
+        // check whether our password and confirm password match
+        validator: CustomValidators.passwordMatchValidator
+      }
+    );
   }
 
   submit() {
     // this.isOtp = true;
-    this.authService.signIn(this.frmSignup.value).subscribe(() => {
-      this.isOtp = true
-    })
+    console.log(this.frmSignup.controls["Password"].value);
+    var salt = bcrypt.genSaltSync(10);
+    console.log("Salt", salt)
+    var hash = bcrypt.hashSync(this.frmSignup.controls["Password"].value, salt);
+    console.log(hash);
   }
 }
